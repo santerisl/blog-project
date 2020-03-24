8375cf5742ad719c5e15412e24629b0ef4c5cadb
Santeri Saraluhta <santerisaraluhta@gmail.com>
2020-03-24 08:51:17 +0200

    Add test data on app launch

--- src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostController.java
+++ src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostController.java
@@ -3,18 +3,31 @@ package fi.tuni.tiko.helpdesk.blog.blog.BlogPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
+import javax.annotation.PostConstruct;
+
 @RestController
 public class BlogPostController {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
 
-    @PostMapping(value = "/blog/add")
+    @PostConstruct
+    public void initialize() {
+        for(int i = 0; i < 10; i++) {
+            BlogPost p = new BlogPost("Author", "Title " + i, "Content", 1,
+                    LocalDateTime.now(), null);
+                blogPostRepository.save(p);
+        }
+    }
+    
+    @PostMapping(value = "/api/posts")
     public void addBlogPost(@RequestBody BlogPost post) {
         blogPostRepository.save(post);
     }
 
-    @GetMapping(value = "/blog/all")
+    @GetMapping(value = "/api/posts/")
     public Iterable<BlogPost> getAllBlogPosts() {
         return blogPostRepository.findAll();
     }
