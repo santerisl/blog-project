c24843204f73a0bfbf59b2c30bf46a2369b296b7
Wurzsag <ekku.poh@gmail.com>
2020-04-14 22:35:30 +0300

    BlogPostController add delete by post id

--- src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostController.java
+++ src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostController.java
@@ -2,6 +2,7 @@ package fi.tuni.tiko.helpdesk.blog.blog.BlogPost;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -9,6 +10,7 @@ import javax.annotation.PostConstruct;
 
 @CrossOrigin
 @RestController
+@RequestMapping(value = "/api")
 public class BlogPostController {
 
     @Autowired
@@ -21,20 +23,30 @@ public class BlogPostController {
         }
     }
     
-    @PostMapping(value = "/api/posts/")
+    @PostMapping(value = "/posts/")
     public void addBlogPost(@RequestBody BlogPost post) {
         blogPostRepository.save(post);
     }
 
-    @GetMapping(value = "/api/posts/")
+    @GetMapping(value = "/posts/")
     public Iterable<BlogPost> getAllBlogPosts() {
         return blogPostRepository.findAllByOrderByDateDesc();
     }
 
-    @GetMapping(value = "/api/posts/{blogId}")
+    @GetMapping(value = "/posts/{blogId}")
     public BlogPost getBlogPost(@PathVariable long blogId) {
         try {
             return blogPostRepository.findById(blogId).get();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No blog post with id: " + blogId + ".", ex);
+        }
+    }
+
+    @DeleteMapping(value = "/posts/{blogId}")
+    public ResponseEntity<Void> deleteBlogPost(@PathVariable long blogId) {
+        try {
+            blogPostRepository.deleteById(blogId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No blog post with id: " + blogId + ".", ex);
         }
