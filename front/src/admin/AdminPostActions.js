d0cfe9abe06fa624f5247b1915020c37fe7077f5
Santeri Saraluhta <santerisaraluhta@gmail.com>
2020-05-06 14:48:51 +0300

    Move post modify and remove buttons to dropdown menu

--- front/src/admin/AdminPostActions.js
+++ front/src/admin/AdminPostActions.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 import { AdminContext } from './AdminContext'
 import { LinkContainer } from 'react-router-bootstrap'
 
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
 
+
+const style = {
+  top: 0,
+  right: 0,
+  transform: 'translate(25%, -25%)'
+};
 
 async function deletePost(id) {
   const response = await fetch(`/api/posts/${id}`, {
@@ -25,16 +31,21 @@ class AdminPostActions extends Component {
   render() {
     const ctx = this.context
     const isLoggedIn = ctx.user.name !== undefined
-    if(isLoggedIn) {
+    if (isLoggedIn) {
       return (
-        <Card.Footer className="d-flex p-0">
-          <LinkContainer to={`/modify/${this.props.id}`}>
-            <Button className="w-100 rounded-0" size="sm" variant="primary">Modify</Button>
-          </LinkContainer>
-
-          <Button className="w-100 rounded-0" size="sm" variant="danger"
-            onClick={this.onDelete}>Delete</Button>
-        </Card.Footer>
+        <div className="position-absolute" style={style}>
+          <Dropdown drop="down">
+            <Dropdown.Toggle variant="link" className="no-arrow">
+              <Image src="/settings-black.svg" alt="Settings" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu alignRight>
+              <LinkContainer to={`/modify/${this.props.id}`}>
+                <Dropdown.Item>Modify</Dropdown.Item>
+              </LinkContainer>
+              <Dropdown.Item onClick={this.onDelete}>Delete</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       )
     } else {
       return null
