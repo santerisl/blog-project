6a2b0f83069db62123cec2c0c88fb579ab720205
Wurzsag <ekku.poh@gmail.com>
2020-05-11 01:33:14 +0300

    Add missing Javadoc comments

--- src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostProjectionId.java
+++ src/main/java/fi/tuni/tiko/helpdesk/blog/blog/BlogPost/BlogPostProjectionId.java
@@ -1,5 +1,16 @@
 package fi.tuni.tiko.helpdesk.blog.blog.BlogPost;
 
+/**
+ * Projection for blog post. Sends only the needed data for the frontend.
+ * <p>
+ *     Web App Development and Project, 4A00CN42-3004, Spring 2020
+ * </p>
+ * @author Elias Pohjalainen,
+ * Business Information Systems, Tampere University of Applied Sciences.
+ * @author Santeri Saraluhta,
+ * Business Information Systems, Tampere University of Applied Sciences.
+ * @version 1.0
+ */
 public interface BlogPostProjectionId {
 
     long getId();
